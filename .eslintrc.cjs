63ce210c9b0fb7b3011322493f4eadc93079ef2f
WiiJoy MacOS <wiijoy@bk.ru>
2022-08-14 17:52:40 +0300

    feat: update configs for eslint and ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,11 @@ module.exports = {
   "extends": [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
+    "airbnb-base",
+    "airbnb-typescript/base",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
+    "@vue/eslint-config-airbnb-with-typescript"
   ],
   "parserOptions": {
     "ecmaVersion": "latest"
