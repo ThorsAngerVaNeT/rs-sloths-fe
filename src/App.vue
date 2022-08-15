344903fd15bcbe9b080c15b856d32bbf1f043e4b
WiiJoy MacOS <wiijoy@bk.ru>
2022-08-15 18:04:12 +0300

    feat: add husky

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HeaderView from './components/header/HeaderView.vue';
 import FooterView from './components/footer/FooterView.vue';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderView,
     FooterView,
