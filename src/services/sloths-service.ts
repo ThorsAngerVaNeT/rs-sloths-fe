cf1e1d6c0257d9b292725da77dce4b787188848d
ogimly <ogimly@gmail.com>
2022-08-16 23:50:40 +0500

    feat: add implementation for sloths endpoint

---
+++ src/services/sloths-service.ts
@@ -0,0 +1,33 @@
+import type { API } from '@/common/types';
+import { Endpoints } from '../common/enums/endpoints';
+import { APIService } from './api-service';
+
+export class SlothsService implements API {
+  private service = new APIService(Endpoints.sloths);
+
+  public getAll() {
+    return this.service.getAll();
+  }
+
+  public getSome(page: number, limit: number) {
+    return this.service.getSome(page, limit);
+  }
+
+  public getById(id: number) {
+    return this.service.getById(id);
+  }
+
+  public create(body: unknown) {
+    return this.service.create(body);
+  }
+
+  public updateById(id: number, body: unknown) {
+    return this.service.updateById(id, body);
+  }
+
+  public deleteById(id: number) {
+    return this.service.deleteById(id);
+  }
+}
+
+export default SlothsService;
