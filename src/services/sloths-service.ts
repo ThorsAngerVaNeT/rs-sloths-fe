87044eafe68212911e38cb952371b611a0f8b8fc
ogimly <ogimly@gmail.com>
2022-08-17 01:13:50 +0500

    feat: add dummys for services

--- src/services/sloths-service.ts
+++ src/services/sloths-service.ts
@@ -5,28 +5,71 @@ import { APIService } from './api-service';
 export class SlothsService implements API {
   private service = new APIService(Endpoints.sloths);
 
+  private getAllResult = {
+    ok: true,
+    status: 200,
+    data: [
+      {
+        id: 1,
+        name: 'sloth1',
+      },
+      {
+        id: 2,
+        name: 'sloth2',
+      },
+      {
+        id: 3,
+        name: 'sloth3',
+      },
+    ],
+    headers: 3,
+  };
+
+  private getByIdResult = {
+    ok: true,
+    status: 200,
+    data: {
+      id: 1,
+      name: 'sloth1',
+    },
+    headers: '',
+  };
+
+  private getDeleteResult = {
+    ok: true,
+    status: 200,
+    data: {},
+    headers: '',
+  };
+
   public getAll() {
-    return this.service.getAll();
+    // return this.service.getAll();
+    return Promise.resolve(this.getAllResult);
   }
 
   public getSome(page: number, limit: number) {
-    return this.service.getSome(page, limit);
+    // return this.service.getSome(page, limit);
+    return Promise.resolve(this.getAllResult);
   }
 
   public getById(id: number) {
-    return this.service.getById(id);
+    // return this.service.getById(id);
+    return Promise.resolve(this.getByIdResult);
   }
 
   public create(body: unknown) {
-    return this.service.create(body);
+    // return this.service.create(body);
+    return Promise.resolve(this.getByIdResult);
   }
 
   public updateById(id: number, body: unknown) {
-    return this.service.updateById(id, body);
+    // return this.service.updateById(id, body);
+    return Promise.resolve(this.getByIdResult);
   }
 
   public deleteById(id: number) {
-    return this.service.deleteById(id);
+    // return this.service.deleteById(id);
+    return Promise.resolve(this.getDeleteResult);
   }
 }
 
--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -5,28 +5,75 @@ import { APIService } from './api-service';
 export class UsersService implements API {
   private service = new APIService(Endpoints.users);
 
+  private getAllResult = {
+    ok: true,
+    status: 200,
+    data: [
+      {
+        id: 1,
+        name: 'user1',
+        email: 'user1@gmail.com',
+      },
+      {
+        id: 2,
+        name: 'user2',
+        email: 'user2@gmail.com',
+      },
+      {
+        id: 3,
+        name: 'user3',
+        email: 'user3@gmail.com',
+      },
+    ],
+    headers: 3,
+  };
+
+  private getByIdResult = {
+    ok: true,
+    status: 200,
+    data: {
+      id: 1,
+      name: 'user1',
+      email: 'user1@gmail.com',
+    },
+    headers: '',
+  };
+
+  private getDeleteResult = {
+    ok: true,
+    status: 200,
+    data: {},
+    headers: '',
+  };
+
   public getAll() {
-    return this.service.getAll();
+    // return this.service.getAll();
+    return Promise.resolve(this.getAllResult);
   }
 
   public getSome(page: number, limit: number) {
-    return this.service.getSome(page, limit);
+    // return this.service.getSome(page, limit);
+    return Promise.resolve(this.getAllResult);
   }
 
   public getById(id: number) {
-    return this.service.getById(id);
+    // return this.service.getById(id);
+    return Promise.resolve(this.getByIdResult);
   }
 
   public create(body: unknown) {
-    return this.service.create(body);
+    // return this.service.create(body);
+    return Promise.resolve(this.getByIdResult);
   }
 
   public updateById(id: number, body: unknown) {
-    return this.service.updateById(id, body);
+    // return this.service.updateById(id, body);
+    return Promise.resolve(this.getByIdResult);
   }
 
   public deleteById(id: number) {
-    return this.service.deleteById(id);
+    // return this.service.deleteById(id);
+    return Promise.resolve(this.getDeleteResult);
   }
 }
 
