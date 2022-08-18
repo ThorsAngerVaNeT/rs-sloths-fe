8c62761872b4c7ee69d2e12926f3204551d08936
WiiJoy MacOS <wiijoy@bk.ru>
2022-08-18 10:11:36 +0300

    refactor: footer rebuild

--- src/components/footer/FooterView.vue
+++ src/components/footer/FooterView.vue
@@ -1,13 +1,11 @@
 <template>
   <footer class="footer">
-    <a class="link" href="https://rs.school/js/" target="_blank"></a>
+    <a class="rss" href="https://rs.school/js/" target="_blank"></a>
     <div class="team">
-      <div class="team__logo"></div>
-      <div class="team__github">
-        <a class="link" href="https://github.com/WiiJoy" target="_blank">WiiJoy</a>
-        <a class="link" href="https://github.com/Ogimly" target="_blank">Ogimly</a>
-        <a class="link" href="https://github.com/ThorsAngerVaNeT" target="_blank">VaNeT</a>
-      </div>
+      <a class="team__item link" href="https://github.com/WiiJoy" target="_blank">WiiJoy</a>
+      <a class="team__item link" href="https://github.com/Ogimly" target="_blank">Ogimly</a>
+      <a class="team__item link" href="https://github.com/ThorsAngerVaNeT" target="_blank">VaNeT</a>
+      <p class="team__item">&#169; 2022</p>
     </div>
   </footer>
 </template>
@@ -19,3 +17,54 @@ export default defineComponent({
   name: 'FooterView',
 });
 </script>
+
+<style>
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+}
+
+.rss {
+  height: 20px;
+  width: 50px;
+  background: center center / contain url('../../assets/icons/rs-school.png') no-repeat;
+}
+
+.team {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 10px;
+}
+
+.team__logo {
+  height: 40px;
+  width: 71px;
+  background: center center / contain url('../../assets/icons/wwt-light.svg') no-repeat;
+}
+
+.team__github {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  gap: 4px;
+}
+
+.team__item {
+  color: #000;
+  text-decoration: none;
+  font-size: 12px;
+}
+
+.link {
+  transition: .3s;
+}
+
+.link:hover {
+  color: #303030;
+}
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import i18n from './i18n';
 import App from './App.vue';
 import router from './router';
 
-import './assets/main.css';
+import './assets/styles/main.css';
 
 const app = createApp(App);
 
