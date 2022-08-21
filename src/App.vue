2a4e4e615f3e6e1ead30f34ddddf2907c1509712
WiiJoy MacOS <wiijoy@bk.ru>
2022-08-21 13:08:58 +0300

    feat: add loader component

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,21 @@
   </main>
 
   <footer-view />
+
+  <loader-view />
 </template>
 
 <script lang="ts">
 import HeaderView from './components/header/HeaderView.vue';
 import FooterView from './components/footer/FooterView.vue';
+import LoaderView from './components/loader/LoaderView.vue';
 
 export default {
   name: 'App',
   components: {
     HeaderView,
     FooterView,
+    LoaderView,
   },
 };
 </script>
---
+++ src/components/loader/LoaderView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loader">Loader</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoaderView',
+});
+</script>
+
+<style scoped>
+.loader {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  z-index: 1000;
+  background-color: var(--dark-opacity);
+}
+</style>
