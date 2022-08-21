a3499ebdf6524dd4288ef8b2323be22e29549516
WiiJoy MacOS <wiijoy@bk.ru>
2022-08-21 15:01:16 +0300

    feat: add 404-page component

--- src/router/index.ts
+++ src/router/index.ts
@@ -52,6 +52,12 @@ const router = createRouter({
       name: 'admin',
       component: () => import('../views/Admin.vue'),
     },
+    // 404 always last item
+    {
+      path: '/*',
+      name: '404',
+      component: () => import('../views/404.vue'),
+    },
   ],
 });
 
---
+++ src/views/404.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-404">
+    <div class="page-404__image"></div>
+    <h2 class="page-404__title"></h2>
+    <p class="page-404__descr"></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: '404View',
+});
+</script>
+
+<style scoped>
+.page-404 {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.page-404__image {
+  height: 250px;
+  width: 250px;
+  background: no-repeat center center / contain url('../assets/icons/404/404.svg');
+}
+
+.page-404__title {
+  color: var(--color-heading);
+}
+
+.page-404__descr {
+  color: var(--color-text);
+}
+</style>
