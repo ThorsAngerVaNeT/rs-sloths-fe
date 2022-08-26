60fa48f44758b7cdf0325f0ef0170e58bd4442b0
ogimly <ogimly@gmail.com>
2022-08-26 16:58:54 +0500

    feat: close authorization go home

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,7 @@
   <loader-view v-show="isLoad" />
 
   <alert-modal v-show="isAlert" :header="header" :message="message" @closeAlertModal="isAlert = false"></alert-modal>
-  <authorization-modal
-    v-show="isAuthorization"
-    @closeAuthorizationModal="isAuthorization = false"
-  ></authorization-modal>
+  <authorization-modal v-show="isAuthorization" @closeAuthorizationModal="closeAuthorization"></authorization-modal>
 </template>
 
 <script lang="ts">
@@ -28,6 +25,7 @@ import AuthorizationModal from './components/modal/AuthorizationModal.vue';
 import useLoader from './stores/loader';
 import useAlertModal from './stores/alert-modal';
 import useAuthorizationModal from './stores/authorization-modal';
+import router from './router';
 
 export default defineComponent({
   name: 'App',
@@ -55,6 +53,12 @@ export default defineComponent({
     setTimeout(() => {
       this.isLoad = false;
     }, 100);
+  },
+  methods: {
+    closeAuthorization() {
+      this.isAuthorization = false;
+      router.push('/');
+    },
   },
 });
 </script>
