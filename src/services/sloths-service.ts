c4cb62fe078fdedd37aa0cfe2a26ac748e2dc3dd
ogimly <ogimly@gmail.com>
2022-08-27 01:07:48 +0500

    feat: del mocks, upd get methods

--- src/services/sloths-service.ts
+++ src/services/sloths-service.ts
@@ -1,42 +1,43 @@
-import type { API, Sloth, SlothRating } from '@/common/types';
+import type { API, Sloth, SlothRating, WhereField, WhereFieldFilter } from '@/common/types';
+import { getORFields, getFieldContainsFilter, getFieldEqualFilter, getANDFields } from '@/utils/query-string';
 import { Endpoints } from '../common/enums/endpoints';
 import { APIService } from './api-service';
-import { getAllSloths, getByIdSloth, getDeleteSloth } from './mocks/sloths-mocks';
+
+const getFilter = (searchText: string, selected: string[]): string => {
+  const search: WhereFieldFilter | WhereField | null = searchText
+    ? getORFields(['caption', 'description'].map((field) => getFieldContainsFilter(field, searchText)))
+    : null;
+
+  const select: WhereFieldFilter | WhereField | null = selected.length
+    ? getORFields(selected.map((field) => getFieldEqualFilter('role', field)))
+    : null;
+
+  return getANDFields([search, select]);
+};
 
 export class SlothsService implements API<Sloth> {
   private service = new APIService<Sloth>(Endpoints.sloths);
 
-  private getAllResult = getAllSloths;
-
-  private getByIdResult = getByIdSloth;
-
-  private getDeleteResult = getDeleteSloth;
-
-  public getAll() {
-    return this.service.getAll();
-    // return Promise.resolve(this.getAllResult);
+  public getAll(searchText = '', sorting = '', selected = [] as string[]) {
+    return this.service.getAll(getFilter(searchText, selected), sorting);
   }
 
-  public getPage(page: number, limit: number) {
-    return this.service.getPage(page, limit);
-    // return Promise.resolve(this.getAllResult);
+  public getPage(page: number, limit: number, searchText = '', sorting = '', selected = [] as string[]) {
+    return this.service.getPage(page, limit, getFilter(searchText, selected), sorting);
   }
 
   public getById(id: string) {
     return this.service.getById(id);
-    // return Promise.resolve(this.getByIdResult);
   }
 
   public create(body: Sloth) {
     return this.service.create(body);
-    // return Promise.resolve(this.getByIdResult);
   }
 
   public updateById(slothId: string, sloth: Sloth) {
     const { id, caption, description } = sloth;
     const body = { id, caption, description };
     return this.service.updateById(slothId, body);
-    // return Promise.resolve(this.getByIdResult);
   }
 
   public static updateRatingById(slothId: string, rate: number) {
@@ -49,7 +50,6 @@ export class SlothsService implements API<Sloth> {
 
   public deleteById(id: string) {
     return this.service.deleteById(id);
-    // return Promise.resolve(this.getDeleteResult);
   }
 }
 
