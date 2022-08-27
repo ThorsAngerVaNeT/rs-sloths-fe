9803ba9cab24828dbdcd7dd6720e0fe1d7fd1170
WiiJoy MacOS <wiijoy@bk.ru>
2022-08-27 15:12:51 +0300

    feat: create temporary about section

---
+++ src/components/about/AboutSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="about__section">
+    <h2 class="about__title">О RS SLOTHS</h2>
+    <div class="about__project">
+      <p class="about__main">Все ленивцы в одном месте!</p>
+      <div class="about__descr">
+        Мы собрали для Вас всех ленивцев вместе! Теперь достаточно лишь зайти в каталог, выбрать необходимых и скачать
+        их. Есть идеи, но нет таких ленивцев? Предложи! Это может быть уже готовый ленивец, рисунок-набросок или просто
+        текстовое описание - и если идея найдет отклик у почитателей ленивцев, она обязательно воплотится в жизнь!
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AboutSection',
+});
+</script>
