b5a5e1fc44f29653f41cec029e0987c8ccf7263e
ogimly <ogimly@gmail.com>
2022-08-29 02:34:26 +0500

    feat: add alert on del user

--- src/components/admin/UserCard.vue
+++ src/components/admin/UserCard.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="user-info__btn">
       <custom-btn className="btn btn-icon icon-edit" @click="$emit('editUser', userInfo)"></custom-btn>
-      <custom-btn className="btn btn-icon icon-del" @click="$emit('delUser', userInfo.id)"></custom-btn>
+      <custom-btn className="btn btn-icon icon-del" @click="delItem"></custom-btn>
     </div>
     <div class="user-info__props">
       <p class="user-info__property">{{ $t('profile.name') }}</p>
@@ -19,6 +19,18 @@
       <p class="user-info__property">{{ userInfo.role }}</p>
       <p class="user-info__property">{{ new Date(userInfo.createdAt).toLocaleDateString() }}</p>
     </div>
+    <modal-window v-show="isApproveShow" @close="closeModal">
+      <template v-slot:header> {{ $t('modal.header.alert') }} </template>
+
+      <template v-slot:body> {{ $t('modal.body.del-user') }} </template>
+
+      <template v-slot:footer>
+        <div class="user-info__btn btn-horizontal">
+          <custom-btn :text="$t('btn.yes')" className="btn btn-primary" :onClick="approveDelItem"></custom-btn>
+          <custom-btn :text="$t('btn.no')" className="btn btn-primary" :onClick="closeModal"></custom-btn>
+        </div>
+      </template>
+    </modal-window>
   </div>
 </template>
 
@@ -27,12 +39,18 @@ import { defineComponent, type PropType } from 'vue';
 import type { User } from '@/common/types';
 import { DEFAULT_USER_AVATAR } from '@/common/const';
 import CustomBtn from '@/components/buttons/CustomBtn.vue';
+import ModalWindow from '@/components/modal/ModalWindow.vue';
 
 export default defineComponent({
   name: 'UserCard',
 
   components: {
     CustomBtn,
+    ModalWindow,
+  },
+
+  data() {
+    return { isApproveShow: false };
   },
 
   props: {
@@ -45,6 +63,19 @@ export default defineComponent({
   computed: {
     getAvatar(): string {
       return this.userInfo.avatar_url ?? DEFAULT_USER_AVATAR;
+    },
+  },
+
+  methods: {
+    delItem() {
+      this.isApproveShow = true;
+    },
+    approveDelItem() {
+      this.$emit('delUser', this.userInfo.id);
+      this.closeModal();
+    },
+    closeModal() {
+      this.isApproveShow = false;
     },
   },
 });
@@ -91,6 +122,9 @@ export default defineComponent({
   align-items: center;
   justify-content: center;
   gap: var(--gap);
+}
+.btn-horizontal {
+  flex-direction: row;
 }
 
 @media (max-width: 1000px) {
