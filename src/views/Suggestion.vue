d69cdf0daf78551af594e04768d3400b7ab32a78
ogimly <ogimly@gmail.com>
2022-08-30 11:26:34 +0500

    fix: del mock

--- src/views/Suggestion.vue
+++ src/views/Suggestion.vue
@@ -114,21 +114,7 @@ export default defineComponent({
   },
 
   mounted() {
-    // this.getSuggestions();
-    this.suggestions = [
-      {
-        id: 'cd86722d-e3cc-405c-9a46-8da7d7d2dfcf',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        image_url: 'http://localhost:5173/test01.svg',
-        // image_url: '',
-        userId: 'cd86722d-e3cc-405c-9a46-8da7d7d2dfcf',
-        rating: 3,
-        createdAt: new Date('2022-08-29T17:32:47.207Z'),
-        status: SuggestionStatus.pending,
-      },
-    ];
-    this.count = 1;
+    this.getSuggestions();
   },
 
   methods: {
